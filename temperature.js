164d54d88692581d38364f8ab19328ecc2bf2886
Swamp Temp <swamptemp@gmail.com>
2017-09-24 20:00:17 -0400

    Update live temperature on site

--- temperature.js
+++ temperature.js
@@ -1,1 +1,1 @@
-document.write("<h4>Inside Temperature: 26.0°C</h4><h4>Outside Temperature: 26.93°C</h4><h4>Fermenter 1 Temperature: 18.69°C</h4><h4>Fermenter 2 Temperature: 16.44°C</h4><h4>Inside Humidity: 86.56%</h4><h4>Outside Humidity: 63.0%</h4><br>Last update: 2017-09-24 17:51:31");
+document.write("<h4>Inside Temperature: 25.11°C</h4><h4>Outside Temperature: 22.97°C</h4><h4>Fermenter 1 Temperature: 19.56°C</h4><h4>Fermenter 2 Temperature: 16.38°C</h4><h4>Inside Humidity: 70.47%</h4><h4>Outside Humidity: 78.0%</h4><br>Last update: 2017-09-24 19:51:36");
